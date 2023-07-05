e5d0615e6449d875d8e8b9e33c525cbdee503adb
alanfedrizzi <alanfedrizzi@gmail.com>
2023-07-05 16:19:57 +0200

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import { resolve } from 'path';
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [splitVendorChunkPlugin()],
-  base: '/templarios-shift',
+  base: "",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        research: resolve(__dirname, 'src/pages/research/research.html'),
-        about: resolve(__dirname, 'src/pages/about/about.html'),
-        projects: resolve(__dirname, 'src/pages/projects/projects.html')
+        main: resolve(__dirname, "index.html"),
+        research: resolve(__dirname, "src/pages/research/research.html"),
+        about: resolve(__dirname, "src/pages/about/about.html"),
+        projects: resolve(__dirname, "src/pages/projects/projects.html"),
       },
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+      // output: {
+      //   entryFileNames: `assets/[name].js`,
+      //   chunkFileNames: `assets/[name].js`,
+      //   assetFileNames: `assets/[name].[ext]`,
+      // },
+    },
+  },
 });
 
 // import { defineConfig, splitVendorChunkPlugin } from "vite";
